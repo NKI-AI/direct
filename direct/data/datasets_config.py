1ef1497464927f7c4991653b6a67bcb5b5b6790d
Jonas Teuwen <jonasteuwen@gmail.com>
2020-06-30 10:24:16 +0200

    Added Calgary Campinas subsample function and dataset

--- direct/data/datasets_config.py
+++ direct/data/datasets_config.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Tuple
 
 from direct.config.defaults import BaseConfig
@@ -8,7 +8,8 @@ from direct.config.defaults import BaseConfig
 
 @dataclass
 class TransformsConfig(BaseConfig):
-    crop: Tuple[int, int] = (320, 320)
+    crop: Tuple[int] = field(default_factory=lambda: (320, 320))
+    estimate_sensitivity_maps: bool = False
 
 
 @dataclass

fdc648076e8c2ea6379635ec15b3ee106719e0b9
Jonas Teuwen <jonasteuwen@gmail.com>
2020-07-29 13:11:38 +0200

    Volume metrics is not used anymore

--- direct/engine.py
+++ direct/engine.py
@@ -90,7 +90,7 @@ class Engine(ABC):
         # TODO: Batch size can be much larger, perhaps have a different batch size during evaluation.
         data_loader = self.build_loader(dataset, batch_sampler=batch_sampler, num_workers=num_workers)
         loss, output = self.evaluate(
-            data_loader, loss_fns=None, volume_metrics=None, crop=None, is_validation_process=False)
+            data_loader, loss_fns=None, crop=None, is_validation_process=False)
 
         return output
 

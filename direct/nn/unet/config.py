89ce39fa8fed3e430d9f9f9f1e261fd731abce73
Jonas Teuwen <jonasteuwen@gmail.com>
2020-08-15 11:32:54 +0200

    Fix config of unet to convention

--- direct/nn/unet/config.py
+++ direct/nn/unet/config.py
@@ -1,15 +1,14 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
 from dataclasses import dataclass
-from typing import Tuple, List, Any
 
 from direct.config.defaults import ModelConfig
 
 
 @dataclass
-class Unet2DConfig(ModelConfig):
-    in_channels: int = 1
-    out_channels: int = 1
-    num_filters: int = 16
+class UnetModel2dConfig(ModelConfig):
+    in_channels: int = 2
+    out_channels: int = 2
+    num_filters: int = 8
     num_pool_layers: int = 4
     dropout_probability: float = 0.0

98a3f8ac0de5b9eef561c661e32f8d8f6d2dcb89
Jonas Teuwen <jonasteuwen@gmail.com>
2020-09-08 21:22:00 +0200

    Cosmetic changes

--- direct/nn/unet/config.py
+++ direct/nn/unet/config.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
 from dataclasses import dataclass
-
 from direct.config.defaults import ModelConfig
 
 
@@ -9,6 +8,6 @@ from direct.config.defaults import ModelConfig
 class UnetModel2dConfig(ModelConfig):
     in_channels: int = 2
     out_channels: int = 2
-    num_filters: int = 8
+    num_filters: int = 16
     num_pool_layers: int = 4
     dropout_probability: float = 0.0

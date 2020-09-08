bd33bbed3bf9440e435f67e2ca7aa9ee38c8837c
Jonas Teuwen <jonasteuwen@gmail.com>
2020-09-08 21:26:40 +0200

    Updated RIM config

---
+++ direct/nn/mobilenet/config.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# Copyright (c) DIRECT Contributors
+from dataclasses import dataclass
+from direct.config.defaults import ModelConfig
+
+#
+# @dataclass
+# class MobileNetV2Config(ModelConfig):
+#     num_classes: int = 1000
+#     width_mult: float = 1.0,
+#     inverted_residual_setting = None,
+#     round_nearest: int = 8,
+#     block = None,
+#     norm_layer = None

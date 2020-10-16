3f6b2cd5f250c071e22900e6a780a725d1b5d205
Jonas Teuwen <jonasteuwen@gmail.com>
2020-10-16 19:22:00 +0200

    Closes #12

---
+++ direct/utils/models.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# Copyright (c) DIRECT Contributors
+from collections import OrderedDict
+
+
+def fix_state_dict_module_prefix(state_dict):
+    """
+    If models are saved after being wrapped in e.g. DataParallel,
+    the keys of the state dict are prefixed with `module.`.
+    This function removes this prefix.
+
+    Parameters
+    ----------
+    state_dict : dict
+        state_dict of a network module
+    Returns
+    -------
+    dict
+
+    """
+    if list(state_dict.keys())[0].startswith("module."):
+        new_ordered_dict = OrderedDict()
+        for idx, (k, v) in enumerate(state_dict.items()):
+            name = k[7:]
+            new_ordered_dict[name] = v
+        state_dict = new_ordered_dict
+
+    return state_dict

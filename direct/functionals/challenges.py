255991e5e7e497b4f4952d776afc76177d17d3a6
Jonas Teuwen <jonasteuwen@gmail.com>
2020-10-19 14:49:49 +0200

    target, volume in the metrics, rather than volume, target

--- direct/functionals/challenges.py
+++ direct/functionals/challenges.py
@@ -34,54 +34,53 @@ def fastmri_ssim(gt, target):
     return torch.from_numpy(np.array(out)).float()
 
 
-def fastmri_psnr(gt, target):
+def fastmri_psnr(gt, pred):
+    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
     gt = _to_numpy(gt)[:, 0, ...]
-    target = _to_numpy(target)[:, 0, ...]
+    pred = _to_numpy(pred)[:, 0, ...]
     from skimage.measure import compare_psnr
 
-    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
-
-    out = compare_psnr(gt, target, data_range=gt.max())
+    out = compare_psnr(gt, pred, data_range=gt.max())
     return torch.from_numpy(np.array(out)).float()
 
 
-def fastmri_nmse(gt, target):
+def fastmri_nmse(gt, pred):
     gt = _to_numpy(gt)[:, 0, ...]
-    target = _to_numpy(target)[:, 0, ...]
-    out = np.linalg.norm(gt - target) ** 2 / np.linalg.norm(gt) ** 2
+    pred = _to_numpy(pred)[:, 0, ...]
+    out = np.linalg.norm(gt - pred) ** 2 / np.linalg.norm(gt) ** 2
     return torch.from_numpy(np.array(out)).float()
 
 
-def _calgary_campinas_metric(gt, target, metric_func):
+def _calgary_campinas_metric(gt, pred, metric_func):
     gt = _to_numpy(gt)[:, 0, ...]
-    target = _to_numpy(target)[:, 0, ...]
+    pred = _to_numpy(pred)[:, 0, ...]
 
     output = []
-    for idx in range(target.shape[0]):
-        data_range = np.maximum(gt[idx].max(), target[idx].max()) - np.minimum(
-            gt[idx].min(), target[idx].min()
+    for idx in range(gt.shape[0]):
+        data_range = np.maximum(gt[idx].max(), pred[idx].max()) - np.minimum(
+            gt[idx].min(), pred[idx].min()
         )
-        output.append(metric_func(gt[idx], target[idx], data_range=data_range))
+        output.append(metric_func(gt[idx], pred[idx], data_range=data_range))
 
     return torch.from_numpy(np.asarray(output)).mean()
 
 
-def calgary_campinas_ssim(gt, target):
+def calgary_campinas_ssim(gt, pred):
     from skimage.metrics import structural_similarity
 
-    return _calgary_campinas_metric(gt, target, structural_similarity)
+    return _calgary_campinas_metric(gt, pred, structural_similarity)
 
 
-def calgary_campinas_psnr(gt, target):
+def calgary_campinas_psnr(gt, pred):
     from skimage.metrics import peak_signal_noise_ratio
 
-    return _calgary_campinas_metric(gt, target, peak_signal_noise_ratio)
+    return _calgary_campinas_metric(gt, pred, peak_signal_noise_ratio)
 
 
-def calgary_campinas_vif(gt, target):
+def calgary_campinas_vif(gt, pred):
     def vif_func(gt, target, data_range):
         from sewar.full_ref import vifp
 
         return vifp(gt, target, sigma_nsq=0.4)
 
-    return _calgary_campinas_metric(gt, target, vif_func)
+    return _calgary_campinas_metric(gt, pred, vif_func)

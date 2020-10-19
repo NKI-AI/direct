22d8c827a71073f06ab35f298d41b7d4bae1e7d8
Jonas Teuwen <jonasteuwen@gmail.com>
2020-10-19 13:32:13 +0200

    fastmri metrics renamed to challenge metrics

--- direct/functionals/__init__.py
+++ direct/functionals/__init__.py
@@ -4,11 +4,11 @@ import numpy as np
 
 from direct.functionals.psnr import *
 from direct.functionals.ssim import *
-from direct.functionals.fastmri import *
+from direct.functionals.challenges import *
 
 __all__ = ()
 __all__ += psnr.__all__
 __all__ += ssim.__all__
-__all__ += fastmri.__all__
+__all__ += challenges.__all__
 
 from direct.functionals.regularizer import body_coil

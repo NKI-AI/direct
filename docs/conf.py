7d1217e4cb19dda91445575bb546b141aee75622
Jonas Teuwen <j.teuwen@nki.nl>
2021-05-23 15:45:11 +0200

    Update documentation - Change theme - Move to docs.aiforoncology.nl/direct

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 #
-# dlup documentation build configuration file, created by
-# sphinx-quickstart on Fri Jun  9 13:47:02 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.

7b22fbf2218491530824f9b02a85cedb84e59eed
George Yiasemis <g.yiasemis@nki.nl>
2021-09-03 17:40:50 +0200

    Added VarNet implementation

---
+++ direct/nn/varnet/config.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# Copyright (c) DIRECT Contributors
+from dataclasses import dataclass
+
+from direct.config.defaults import ModelConfig
+
+
+@dataclass
+class EndToEndVarNetConfig(ModelConfig):
+    num_layers: int = 8
+    regularizer_num_filters: int = 18
+    regularizer_num_pull_layers: int = 4
+    regularizer_dropout: float = 0.0

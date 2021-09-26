b9b7dc4ac63afe84a84f4c6d37a4c50261107b30
George Yiasemis <g.yiasemis@nki.nl>
2021-09-26 14:23:28 +0200

    XPDnet works with more kspace models

--- direct/nn/xpdnet/config.py
+++ direct/nn/xpdnet/config.py
@@ -11,6 +11,13 @@ class XPDNetConfig(ModelConfig):
     num_primal: int = 5
     num_dual: int = 1
     num_iter: int = 10
+    use_primal_only: bool = True
+    kspace_model_architecture: str = "CONV"
+    dual_conv_hidden_channels: int = 16
+    dual_conv_n_convs: int = 4
+    dual_didn_hidden_channels: int = 64
+    dual_didn_num_dubs: int = 6
+    dual_didn_num_convs_recon: int = 9
     mwcnn_hidden_channels: int = 16
     mwcnn_num_scales: int = 4
     mwcnn_bias: bool = True

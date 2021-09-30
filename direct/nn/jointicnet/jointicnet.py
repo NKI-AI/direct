Minor scaling fix

direct/nn/jointicnet/jointicnet.py
@@ -68,6 +68,7 @@ class JointICNet(nn.Module):
             num_pool_layers=kwargs.get("sens_unet_num_pool_layers", 4),
             dropout_probability=kwargs.get("sens_unet_dropout", 0.0),
         )
+        self.conv_out = nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1)
 
         self.reg_param_I = nn.Parameter(torch.ones(num_iter))
         self.reg_param_F = nn.Parameter(torch.ones(num_iter))
@@ -145,7 +146,7 @@ class JointICNet(nn.Module):
 
         input_image = self._backward_operator(masked_kspace, sampling_mask, sensitivity_map)
         scaling_factor = T.modulus(input_image).unsqueeze(self._coil_dim).amax(dim=self._spatial_dims)
-        input_image /= scaling_factor.view(-1, 1, 1, 1)
+        input_image = input_image / scaling_factor.view(-1, 1, 1, 1)
 
         for iter in range(self.num_iter):
 
@@ -200,8 +201,8 @@ class JointICNet(nn.Module):
                 )
             )
             step_image_scale = T.modulus(step_image).unsqueeze(self._coil_dim).amax(dim=self._spatial_dims)
-            step_image /= step_image_scale.view(-1, 1, 1, 1)
+            input_image = input_image / scaling_factor.view(-1, 1, 1, 1)
 
             input_image = input_image - step_image
 
-        return input_image
+        return self.conv_out(input_image.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
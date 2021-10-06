b9c6a3cccf69f131a7445f65c44546c63b669d7b
George Yiasemis <George Yiasemis>
2021-10-06 18:36:29 +0200

    LPD configuration

--- direct/nn/lpd/config.py
+++ direct/nn/lpd/config.py
@@ -7,8 +7,25 @@ from direct.config.defaults import ModelConfig
 
 
 @dataclass
-class LPDNet(ModelConfig):
-    num_iters: int = 25
+class LPDNetConfig(ModelConfig):
+    num_iter: int = 25
     num_primal: int = 5
     num_dual: int = 5
-    n_hidden: int = 32
+    primal_model_architecture: str = "MWCNN"
+    dual_model_architecture: str = "DIDN"
+    primal_mwcnn_hidden_channels: int = 16
+    primal_mwcnn_num_scales: int = 4
+    primal_mwcnn_bias: bool = True
+    primal_mwcnn_batchnorm: bool = False
+    primal_unet_num_filters: int = 8
+    primal_unet_num_pool_layers: int = 4
+    primal_unet_dropout_probability: float = 0.0
+    dual_conv_hidden_channels: int = 16
+    dual_conv_n_convs: int = 4
+    dual_conv_batchnorm: bool = False
+    dual_didn_hidden_channels: int = 64
+    dual_didn_num_dubs: int = 6
+    dual_didn_num_convs_recon: int = 9
+    dual_unet_num_filters: int = 8
+    dual_unet_num_pool_layers: int = 4
+    dual_unet_dropout_probability: float = 0.0
--- direct/nn/lpd/lpd.py
+++ direct/nn/lpd/lpd.py
@@ -4,6 +4,10 @@
 from typing import Callable, Optional
 
 import direct.data.transforms as T
+from direct.nn.conv.conv import Conv2d
+from direct.nn.didn.didn import DIDN
+from direct.nn.mwcnn.mwcnn import MWCNN
+from direct.nn.unet.unet_2d import UnetModel2d, NormUnetModel2d
 
 import torch
 import torch.nn as nn
@@ -30,15 +34,24 @@ class DualNet(nn.Module):
         else:
             self.dual_block = kwargs.get("dual_architectue")
 
+    def compute_model_per_coil(self, model, data):
+        """
+        Computes model per coil.
+        """
+        output = []
+
+        for idx in range(data.size(1)):
+            subselected_data = data.select(1, idx)
+            output.append(model(subselected_data))
+        output = torch.stack(output, dim=1)
+
+        return output
+
     def forward(self, h, forward_f, g):
 
         inp = torch.cat([h, forward_f, g], dim=-1).permute(0, 1, 4, 2, 3)
 
-        batch, coil, complex, height, width = inp.size()
-
-        inp = inp.reshape(batch * coil, complex, height, width)
-
-        return self.dual_block(inp).reshape(batch, coil, -1, height, width).permute(0, 1, 3, 4, 2)
+        return self.compute_model_per_coil(self.dual_block, inp).permute(0, 1, 3, 4, 2)
 
 
 class PrimalNet(nn.Module):
@@ -78,42 +91,108 @@ class LPDNet(nn.Module):
         self,
         forward_operator: Callable,
         backward_operator: Callable,
-        num_iters: int,
+        num_iter: int,
         num_primal: int,
         num_dual: int,
-        n_hidden: int = 32,
+        primal_model_architecture: str = "MWCNN",
+        dual_model_architecture: str = "DIDN",
+        **kwargs,
     ):
         """
 
-        :param forward_operator: Callable
-                    Forward operator.
-        :param backward_operator: Callable
-                    Backward operator.
-        :param num_iters: int
-                    Number of unrolled iterations.
-        :param num_primal: int
-                    Number of primal networks.
-        :param num_dual: int
-                    Number of dual networks.
-        :param n_hidden: int
-                    Number of convolutional hidden channels.
+        :param forward_operator: Callable,
+                Forward Operator.
+        :param backward_operator: Callable,
+                Backward Operator.
+        :param num_iter: int,
+                Number of unrolled iterations.
+        :param num_primal: int,
+                Number of primal networks.
+        :param num_dual: int,
+                Number of dual networks.
+        :param primal_model_architecture: str,
+                Primal model architecture. Currently only implemented for MWCNN and (NORM)UNET. Default: 'MWCNN'.
+        :param dual_model_architecture: str,
+                Dual model architecture. Currently only implemented for CONV and DIDN and (NORM)UNET. Default: 'DIDN'.
+        :param kwargs:
+                Keyword arguments for model architectures.
         """
 
         super(LPDNet, self).__init__()
 
         self.forward_operator = forward_operator
         self.backward_operator = backward_operator
-        self.num_iters = num_iters
+        self.num_iter = num_iter
         self.num_primal = num_primal
         self.num_dual = num_dual
-        self.n_hidden = n_hidden
 
-        self.primal_net = nn.ModuleList([PrimalNet(num_primal, n_hidden=n_hidden) for _ in range(num_iters)])
-        self.dual_net = nn.ModuleList([DualNet(num_dual, n_hidden=n_hidden) for _ in range(num_iters)])
+        if primal_model_architecture == "MWCNN":
+            primal_model = nn.Sequential(
+                *[
+                    MWCNN(
+                        input_channels=2 * (num_primal + 1),
+                        first_conv_hidden_channels=kwargs.get("primal_mwcnn_hidden_channels", 32),
+                        num_scales=kwargs.get("primal_mwcnn_num_scales", 4),
+                        bias=kwargs.get("primal_mwcnn_bias", False),
+                        batchnorm=kwargs.get("primal_mwcnn_batchnorm", False),
+                    ),
+                    nn.Conv2d(2 * (num_primal + 1), 2 * num_primal, kernel_size=1),
+                ]
+            )
+        elif primal_model_architecture in ["UNET", "NORMUNET"]:
+            unet = UnetModel2d if primal_model_architecture == "UNET" else NormUnetModel2d
+            primal_model = unet(
+                in_channels=2 * (num_primal + 1),
+                out_channels=2 * num_primal,
+                num_filters=kwargs.get("primal_unet_num_filters", 8),
+                num_pool_layers=kwargs.get("primal_unet_num_pool_layers", 4),
+                dropout_probability=kwargs.get("primal_unet_dropout_probability", 0.0),
+            )
+        else:
+            raise NotImplementedError(
+                f"XPDNet is currently implemented only with primal_model_architecture == 'MWCNN', 'UNET' or 'NORMUNET."
+                f"Got {primal_model_architecture}."
+            )
+
+        if dual_model_architecture == "CONV":
+            dual_model = Conv2d(
+                in_channels=2 * (num_dual + 2),
+                out_channels=2 * num_dual,
+                hidden_channels=kwargs.get("dual_conv_hidden_channels", 16),
+                n_convs=kwargs.get("dual_conv_n_convs", 4),
+                batchnorm=kwargs.get("dual_conv_batchnorm", False),
+            )
+        elif dual_model_architecture == "DIDN":
+            dual_model = DIDN(
+                in_channels=2 * (num_dual + 2),
+                out_channels=2 * num_dual,
+                hidden_channels=kwargs.get("dual_didn_hidden_channels", 16),
+                num_dubs=kwargs.get("dual_didn_num_dubs", 6),
+                num_convs_recon=kwargs.get("dual_didn_num_convs_recon", 9),
+            )
+        elif dual_model_architecture in ["UNET", "NORMUNET"]:
+            unet = UnetModel2d if dual_model_architecture == "UNET" else NormUnetModel2d
+            dual_model = unet(
+                in_channels=2 * (num_dual + 2),
+                out_channels=2 * num_dual,
+                num_filters=kwargs.get("dual_unet_num_filters", 8),
+                num_pool_layers=kwargs.get("dual_unet_num_pool_layers", 4),
+                dropout_probability=kwargs.get("dual_unet_dropout_probability", 0.0),
+            )
+        else:
+            raise NotImplementedError(
+                f"XPDNet is currently implemented for dual_model_architecture == 'CONV', 'DIDN',"
+                f" 'UNET' or 'NORMUNET'. Got dual_model_architecture == {dual_model_architecture}."
+            )
 
         self._coil_dim = 1
         self._complex_dim = -1
         self._spatial_dims = (2, 3)
+
+        self.primal_net = nn.ModuleList(
+            [PrimalNet(num_primal, primal_architectue=primal_model) for _ in range(num_iter)]
+        )
+        self.dual_net = nn.ModuleList([DualNet(num_dual, dual_architectue=dual_model) for _ in range(num_iter)])
 
     def _forward_operator(self, image, sampling_mask, sensitivity_map):
 
@@ -145,31 +224,36 @@ class LPDNet(nn.Module):
         masked_kspace: torch.Tensor,
         sensitivity_map: torch.Tensor,
         sampling_mask: torch.Tensor,
-        scaling_factor: Optional[float] = None,
+        scaling_factor: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
 
         input_image = self._backward_operator(masked_kspace, sampling_mask, sensitivity_map)
 
         if scaling_factor is not None:
-            masked_kspace = masked_kspace * scaling_factor
-            input_image = input_image * scaling_factor
+            masked_kspace = masked_kspace * scaling_factor.reshape(-1, 1, 1, 1, 1)
+            input_image = input_image * scaling_factor.reshape(-1, 1, 1, 1)
 
-        kspace_buffer = torch.cat([masked_kspace] * self.num_dual, self._complex_dim).to(masked_kspace.device)
-        image_buffer = torch.cat([input_image] * self.num_primal, self._complex_dim).to(masked_kspace.device)
+        dual_buffer = torch.cat([masked_kspace] * self.num_dual, self._complex_dim).to(masked_kspace.device)
+        primal_buffer = torch.cat([input_image] * self.num_primal, self._complex_dim).to(masked_kspace.device)
 
-        for iter in range(self.num_iters):
+        for iter in range(self.num_iter):
 
             # Dual
-            f_2 = image_buffer[..., 2:4].clone()
-            kspace_buffer = self.dual_net[iter](
-                kspace_buffer, self._forward_operator(f_2, sampling_mask, sensitivity_map), masked_kspace
+            f_2 = primal_buffer[..., 2:4].clone()
+            dual_buffer = self.dual_net[iter](
+                dual_buffer, self._forward_operator(f_2, sampling_mask, sensitivity_map), masked_kspace
             )
 
             # Primal
-            h_1 = kspace_buffer[..., 0:2].clone()
+            h_1 = dual_buffer[..., 0:2].clone()
 
-            image_buffer = self.primal_net[iter](
-                image_buffer, self._backward_operator(h_1, sampling_mask, sensitivity_map)
+            primal_buffer = self.primal_net[iter](
+                primal_buffer, self._backward_operator(h_1, sampling_mask, sensitivity_map)
             )
 
-        return image_buffer[..., 0:2]
+        output = primal_buffer[..., 0:2]
+
+        if scaling_factor is not None:
+            output = output * scaling_factor.reshape(-1, 1, 1, 1)
+
+        return output

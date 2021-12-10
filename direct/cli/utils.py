7ce3d0837a20a314d9aaed7d4bbd770875d6e32f
Jonas Teuwen <j.teuwen@nki.nl>
2021-12-10 16:22:11 +0100

    dir->file

--- direct/cli/utils.py
+++ direct/cli/utils.py
@@ -10,6 +10,6 @@ def file_or_url(path):
     if check_is_valid_url(path):
         return path
     path = pathlib.Path(path)
-    if path.is_dir():
+    if path.is_file():
         return path
     raise argparse.ArgumentTypeError(f"{path} is not a valid file or url.")

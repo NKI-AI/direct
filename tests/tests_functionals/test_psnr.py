22524eaa3c34d083805a8b1e718f724fbb6a25f9
georgeyiasemis <georgeyiasemis@hotmail.com>
2021-12-18 19:22:05 +0100

    Add tests for challenges psnr

--- tests/tests_functionals/test_psnr.py
+++ tests/tests_functionals/test_psnr.py
@@ -8,6 +8,7 @@ from skimage.color import rgb2gray
 from skimage.metrics import peak_signal_noise_ratio
 from sklearn.datasets import load_sample_image
 
+from direct.functionals.challenges import calgary_campinas_psnr, fastmri_psnr
 from direct.functionals.psnr import PSNRLoss
 
 # Load two images and convert them to grayscale
@@ -22,14 +23,14 @@ def test_psnr(image):
     image_noise_batch = []
     single_image_psnr = []
 
-    for sigma in range(0, 101, 2):
+    for sigma in range(1, 5):
         noise = sigma * np.random.rand(*image.shape)
         image_noise = (image + noise).astype(np.float32).clip(0, 255)
 
         psnr_skimage = peak_signal_noise_ratio(image_true=image, image_test=image_noise, data_range=image_noise.max())
 
         image_torch = (torch.from_numpy(image).unsqueeze(0)).float()  # 1, C, H, W
-        image_noise_torch = (torch.from_numpy(image_noise).unsqueeze(0)).float()
+        image_noise_torch = (torch.from_numpy(image_noise).unsqueeze(0)).float()  # 1, C, H, W
 
         image_batch.append(image_torch)
         image_noise_batch.append(image_noise_torch)
@@ -37,8 +38,7 @@ def test_psnr(image):
         psnr_torch = PSNRLoss(reduction="none").forward(image_noise_torch, image_torch)
 
         psnr_torch = psnr_torch.numpy().item()
-        single_image_psnr.append(psnr_torch)
-        print(psnr_torch, psnr_skimage)
+        single_image_psnr.append(psnr_skimage)
         # Assert that single slice psnr matches
         assert np.allclose(psnr_torch, psnr_skimage, atol=5e-4)
 
@@ -50,5 +50,68 @@ def test_psnr(image):
         image_noise_batch,
         image_batch,
     )
-    # Assert that batch psnr matches
     assert np.allclose(psnr_batch, psnr_skimage_batch, atol=5e-4)
+
+
+@pytest.mark.parametrize("image", [flower, china])
+def test_calgary_campinas_psnr(image):
+
+    image_batch = []
+    image_noise_batch = []
+    single_image_psnr = []
+
+    for sigma in range(1, 5):
+        noise = sigma * np.random.rand(*image.shape)
+        image_noise = (image + noise).astype(np.float32).clip(0, 255)
+
+        psnr_skimage = peak_signal_noise_ratio(
+            image_true=image,
+            image_test=image_noise,
+            data_range=np.maximum(image.max(), image_noise.max()) - np.minimum(image.min(), image_noise.min()),
+        )
+
+        image_torch = (torch.from_numpy(image).unsqueeze(0)).float()  # 1, C, H, W
+        image_noise_torch = (torch.from_numpy(image_noise).unsqueeze(0)).float()  # 1, C, H, W
+
+        image_batch.append(image_torch)
+        image_noise_batch.append(image_noise_torch)
+
+        single_image_psnr.append(psnr_skimage)
+
+    psnr_skimage_batch = np.mean(single_image_psnr)
+
+    image_batch = torch.cat(image_batch, dim=0)
+    image_noise_batch = torch.cat(image_noise_batch, dim=0)
+
+    calgary_campinas_psnr_batch = calgary_campinas_psnr(image_batch, image_noise_batch)
+
+    assert np.allclose(calgary_campinas_psnr_batch, psnr_skimage_batch, atol=5e-4)
+
+
+@pytest.mark.parametrize("image", [flower, china])
+def test_fastmri_psnr(image):
+
+    image_batch = []
+    image_noise_batch = []
+    single_image_psnr = []
+
+    for sigma in range(1, 5):
+        noise = sigma * np.random.rand(*image.shape)
+        image_noise = (image + noise).astype(np.float32).clip(0, 255)
+
+        image_torch = (torch.from_numpy(image).unsqueeze(0)).float()  # 1, C, H, W
+        image_noise_torch = (torch.from_numpy(image_noise).unsqueeze(0)).float()  # 1, C, H, W
+
+        image_batch.append(image[None])
+        image_noise_batch.append(image_noise[None])
+
+    image_batch = np.stack(image_batch)
+    image_noise_batch = np.stack(image_noise_batch)
+
+    psnr_skimage_batch = peak_signal_noise_ratio(image_batch, image_noise_batch, data_range=image_batch.max())
+    image_batch_torch = torch.tensor(image_batch)
+    image_noise_batch_torch = torch.tensor(image_noise_batch)
+
+    fastmri_psnr_batch = fastmri_psnr(image_batch_torch, image_noise_batch_torch)
+
+    assert np.allclose(fastmri_psnr_batch, psnr_skimage_batch, atol=5e-4)

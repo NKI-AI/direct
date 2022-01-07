dd61b07ad21981897a3fa7a6aef216527d5e7454
Jonas Teuwen <j.teuwen@nki.nl>
2022-01-07 12:49:13 +0100

    Bump version -> v1.0.0

--- direct/__init__.py
+++ direct/__init__.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
 
-__author__ = """Jonas Teuwen, George Yiasemis"""
-__email__ = "j.teuwen@nki.nl, g.yiasemis@nki.nl"
-__version__ = "1.0.0-dev0"
+__author__ = """direct contributors"""
+__version__ = "1.0.0"

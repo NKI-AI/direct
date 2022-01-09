cd39863668f811ba891eacce016c23543938ced1
Jonas Teuwen <j.teuwen@nki.nl>
2022-01-09 14:07:41 +0100

    FP

--- direct/utils/io.py
+++ direct/utils/io.py
@@ -56,7 +56,7 @@ def read_json(fn: Union[Dict, str, pathlib.Path]) -> Dict:
 
 class ArrayEncoder(json.JSONEncoder):
     # Below pylint ignore to be a false positive: https://github.com/PyCQA/pylint/issues/414
-    def default(self, obj):  # pylint: disable=method-hidden
+    def default(self, obj):  # pylint: disable=method-hidden,arguments-differ
         if isinstance(obj, torch.Tensor):
             obj = obj.numpy()
 

933f7af97c83adb3cb29208650e6c22a12d0e228
George Yiasemis <georgeyiasemis@hotmail.com>
2022-03-29 22:20:23 +0100

    Update checkpointer.py
    Codacy complaints.

--- direct/checkpointer.py
+++ direct/checkpointer.py
@@ -54,7 +54,7 @@ class Checkpointer:
         del checkpointables["model"]
 
         self.model = self._remove_module_attribute(model)
-        for key in checkpointables.keys():
+        for key in checkpointables.copy():
             if re.match(model_regex, key):
                 checkpointables[key] = self._remove_module_attribute(checkpointables[key])
 

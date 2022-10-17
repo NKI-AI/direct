9461aa6e80832321c2611175bc804553c4932b9a
georgeyiasemis <georgeyiasemis@hotmail.com>
2022-10-17 15:37:29 +0200

    Type hinting

--- direct/nn/unet/config.py
+++ direct/nn/unet/config.py
@@ -24,15 +24,6 @@ class NormUnetModel2dConfig(ModelConfig):
 
 
 @dataclass
-class UnetModel2dConfig(ModelConfig):
-    in_channels: int = 2
-    out_channels: int = 2
-    num_filters: int = 16
-    num_pool_layers: int = 4
-    dropout_probability: float = 0.0
-
-
-@dataclass
 class Unet2dConfig(ModelConfig):
     num_filters: int = 16
     num_pool_layers: int = 4

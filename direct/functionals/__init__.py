29874c987adb6809fc3ce0afea0b95f5d3bc40e0
georgeyiasemis <georgeyiasemis@hotmail.com>
2022-12-02 01:56:55 +0100

    Accuracy metric

--- direct/functionals/__init__.py
+++ direct/functionals/__init__.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
+import torch
 
 from direct.functionals.challenges import *
 from direct.functionals.grad import *
@@ -7,3 +8,8 @@ from direct.functionals.nmae import NMAELoss
 from direct.functionals.nmse import *
 from direct.functionals.psnr import *
 from direct.functionals.ssim import *
+
+
+def accuracy_metric(logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    accuracy = (logits.argmax(dim=-1) == labels).float().mean()
+    return accuracy

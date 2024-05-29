5d75e495c06a1d6ff981cb18532719dbfe7d962f
georgeyiasemis <georgeyiasemis@hotmail.com>
2024-05-29 22:15:21 +0200

    Add 3d varsplitnet engine

--- direct/nn/varsplitnet/varsplitnet_engine.py
+++ direct/nn/varsplitnet/varsplitnet_engine.py
@@ -65,3 +65,62 @@ class MRIVarSplitNetEngine(MRIModelEngine):
         output_kspace = None
 
         return output_image, output_kspace
+
+
+class MRIVarSplitNet3DEngine(MRIModelEngine):
+    """MRIVarSplitNet Engine for 3D data."""
+
+    def __init__(
+        self,
+        cfg: BaseConfig,
+        model: nn.Module,
+        device: str,
+        forward_operator: Optional[Callable] = None,
+        backward_operator: Optional[Callable] = None,
+        mixed_precision: bool = False,
+        **models: nn.Module,
+    ):
+        """Inits :class:`MRIVarSplitNet3DEngine`.
+
+        Parameters
+        ----------
+        cfg: BaseConfig
+            Configuration file.
+        model: nn.Module
+            Model.
+        device: str
+            Device. Can be "cuda:{idx}" or "cpu".
+        forward_operator: Callable, optional
+            The forward operator. Default: None.
+        backward_operator: Callable, optional
+            The backward operator. Default: None.
+        mixed_precision: bool
+            Use mixed precision. Default: False.
+        **models: nn.Module
+            Additional models.
+        """
+        super().__init__(
+            cfg,
+            model,
+            device,
+            forward_operator=forward_operator,
+            backward_operator=backward_operator,
+            mixed_precision=mixed_precision,
+            **models,
+        )
+
+        self._spatial_dims = (3, 4)
+
+    def forward_function(self, data: Dict[str, Any]) -> Tuple[torch.Tensor, None]:
+        data["sensitivity_map"] = self.compute_sensitivity_map(data["sensitivity_map"])
+
+        output_image = self.model(
+            masked_kspace=data["masked_kspace"],
+            sampling_mask=data["sampling_mask"],
+            sensitivity_map=data["sensitivity_map"],
+            scaling_factor=None,
+        )  # shape (batch, slice/time, height,  width, complex[=2])
+
+        output_kspace = None
+
+        return output_image, output_kspace

1e693510c1d1c98845c1b96e161c41b07f374b21
georgeyiasemis <georgeyiasemis@hotmail.com>
2024-05-29 20:20:58 +0200

    Add dynamic sampling and kt masks

--- direct/common/subsample_config.py
+++ direct/common/subsample_config.py
@@ -1,20 +1,23 @@
-# coding=utf-8
 # Copyright (c) DIRECT Contributors
+
+from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional
 
 from omegaconf import MISSING
 
 from direct.config.defaults import BaseConfig
+from direct.types import MaskFuncMode, Number
 
 
 @dataclass
 class MaskingConfig(BaseConfig):
     name: str = MISSING
-    accelerations: Tuple[int, ...] = (5,)  # Ideally Union[float, int].
-    center_fractions: Optional[Tuple[float, ...]] = (0.1,)  # Ideally Optional[Tuple[float, ...]]
+    accelerations: tuple[Number, ...] = (5.0,)
+    center_fractions: Optional[tuple[Number, ...]] = (0.1,)
     uniform_range: bool = False
-    image_center_crop: bool = False
+    mode: MaskFuncMode = MaskFuncMode.STATIC
 
-    val_accelerations: Tuple[int, ...] = (5, 10)
-    val_center_fractions: Optional[Tuple[float, ...]] = (0.1, 0.05)
+    val_accelerations: tuple[Number, ...] = (5.0, 10.0)
+    val_center_fractions: Optional[tuple[Number, ...]] = (0.1, 0.05)

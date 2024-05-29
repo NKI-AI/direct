d0ce5bda0da9b40371b66376de75a1a97c172a12
georgeyiasemis <georgeyiasemis@hotmail.com>
2024-05-29 21:52:49 +0200

    Minor config fix

--- direct/common/subsample_config.py
+++ direct/common/subsample_config.py
@@ -8,16 +8,16 @@ from typing import Optional
 from omegaconf import MISSING
 
 from direct.config.defaults import BaseConfig
-from direct.types import MaskFuncMode, Number
+from direct.types import MaskFuncMode
 
 
 @dataclass
 class MaskingConfig(BaseConfig):
     name: str = MISSING
-    accelerations: tuple[Number, ...] = (5.0,)
-    center_fractions: Optional[tuple[Number, ...]] = (0.1,)
+    accelerations: tuple[float, ...] = (5.0,)
+    center_fractions: Optional[tuple[float, ...]] = (0.1,)
     uniform_range: bool = False
     mode: MaskFuncMode = MaskFuncMode.STATIC
 
-    val_accelerations: tuple[Number, ...] = (5.0, 10.0)
-    val_center_fractions: Optional[tuple[Number, ...]] = (0.1, 0.05)
+    val_accelerations: tuple[float, ...] = (5.0, 10.0)
+    val_center_fractions: Optional[tuple[float, ...]] = (0.1, 0.05)

f83b7bbbbf6de13f16d141a8731d1cd52a7fb834
George Yiasemis <georgeyiasemis@hotmail.com>
2026-06-05 17:36:35 +0200

    fix: cast tensors to float in dict_to_device
    * fix: cast float tensors to float32 before moving to device

--- direct/utils/__init__.py
+++ direct/utils/__init__.py
@@ -172,7 +172,11 @@ def dict_to_device(
     """
     if keys is None:
         keys = data.keys()
-    return {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in data.items() if k in keys}
+    return {
+        k: (v.float() if v.dtype == torch.float64 else v).to(device) if isinstance(v, torch.Tensor) else v
+        for k, v in data.items()
+        if k in keys
+    }
 
 
 def detach_dict(data: Dict[str, torch.Tensor], keys: Optional[Union[List, Tuple, KeysView]] = None) -> Dict:
